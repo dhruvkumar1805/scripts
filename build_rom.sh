2a0e53e7846b7e7f99f4697b636bbccab77b5d89
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-03-16 20:27:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -46,21 +46,7 @@ tg_post_msg "Build Finished..." "$CHATID"
 
 		ZIP="out/target/product/ysl/*zip"
 
-		# Upload the ROM to google drive if it's available, else upload to transfer.sh
-		if [ -x "$(command -v gdrive)" ]; then
+		# Upload the ROM to google drive
 			echo -e "Uploading ROM to Google Drive using gdrive CLI ..."
-			# In some cases when the gdrive CLI is not set up properly, upload fails.
-			# In that case upload it to transfer.sh itself
-			if ! gdrive upload --share -p 1-2KH9G_xwIQ9dqkBirrWl2rd2mBON0aF $ZIP; then
-				echo -e "\nAn error occured while uploading to Google Drive."
-				echo "Uploading ROM zip to transfer.sh..."
-				echo "ROM zip uploaded succesfully: $(curl -sT "$zippath" https://transfer.sh/"$(basename "$zippath")")"
-			fi
-		else
-			echo "Uploading ROM zip to transfer.sh..."
-			echo "ROM zip uploaded succesfully: $(curl -sT "$zippath" https://transfer.sh/"$(basename "$zippath")")"
-		fi
-		echo -e "\n Good bye!"
-		exit 0
-	else echo -e "\n $MAKE_TARGET compiled succesfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) :-) Good bye! \n"; fi
-else echo -e "\nERROR OCCURED DURING COMPILATION :'( EXITING ... \n"; exit 1; fi
+	                tg_post_msg "Uploading Build....." "$CHATID"
+			gdrive upload --share -p 1-2KH9G_xwIQ9dqkBirrWl2rd2mBON0aF $ZIP
