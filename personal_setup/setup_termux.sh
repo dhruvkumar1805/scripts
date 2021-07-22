1ca6de0323182ec636245a4ad4b5437bb0839a29
DhruvChhura <dhruvchhura18@gmail.com>
2021-07-22 10:11:19 +0530

    Create setup_termux.sh

---
+++ personal_setup/setup_termux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Colour setup
+yellow='\033[0;33m'
+white='\033[0m'
+red='\033[0;31m'
+green='\e[0;32m'
+
+clear
+
+# Git credentials
+echo -e "$green Setting Up Git Credentials \n $white"
+git config --global user.name "DhruvChhura"
+git config --global user.email "dhruvchhura18@gmail.com"
+
+# install packages
+echo -e "$green Installing Packages..\n $white"
+
+pkg install git zip tsu wget openssh figlet python p7zip curl build-essential vim -y
+
+echo -e "$yellow Done! \n $white"
