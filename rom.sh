ad30854be4e20ad37bfe5357aa8279c8a0baef3a
DhruvChhura <dhruvchhura18@gmail.com>
2021-10-30 18:50:02 +0530

    rom.sh: Some improvements and fixes

--- rom.sh
+++ rom.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-# ROM
-FNAME="" # This is for filename
-ROM="" # This is for build
-DEVICE="" # Device Codename
-TARGET="" # Build type: user/userdebug/eng
-VERSION="" # Android version
-
-# TELEGRAM BOT
-CHATID="" # Fill Chat Id Of Telegram Group/Channel
-API_BOT="" # Fill API Id Of Bot From BotFater On Telegram
-
-# Init
-FOLDER="${PWD}"
-OUT="${FOLDER}/out/target/product/$DEVICE"
-ERROR_LOG="out/error.log"
+# Build info.
+ROM_NAME="" # Name of the ROM you are compiling e.g. LineageOS
+LUNCH="" # Lunch command e.g. lineage_ysl-userdebug
+MAKE_TARGET="" # Compilation target. e.g. bacon or kernel
+CHATID="" # Your telegram group/channel chatid
+API_BOT="" # Your HTTP API bot token
 
 # Setup Telegram Env
 export BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
 export BOT_BUILD_URL="https://api.telegram.org/bot$API_BOT/sendDocument"
 
-tg_post_msg() {
+message() {
         curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
         -d "parse_mode=html" \
         -d text="$1"
 }
 
-tg_error() {
+error() {
         curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
         -F chat_id="$2" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html"
 }
 
+if [[ $LUNCH == "" ]]; then
+	echo "BRUH: Lunch command not specified! Exiting. . ."
+	exit 1
+fi
+
+DEVICE="$(sed -e "s/^.*_//" -e "s/-.*//" <<< "$LUNCH")"
+BUILD_DATE="$(TZ=Asia/Kolkata date)"
+OUT="$(pwd)/out/target/product/$DEVICE"
+ERROR_LOG="out/error.log"
+
 # Setup transfer.sh
 up(){
 	curl --upload-file $1 https://transfer.sh/
 }
 
-# CleanUp
+# Clean old builds/logs if found
 cleanup() {
     if [ -f "$OUT"/*2021*.zip ]; then
         rm "$OUT"/*2021*.zip
@@ -61,35 +62,33 @@ upload() {
 	echo "zip"
 	END=$(TZ=Asia/Kolkata date +"%s")
  	DIFF=$(( END - START ))
-	tg_post_msg  "<b>Build took *$((DIFF / 60))* minute(s) and *$((DIFF % 60))* second(s)</b>%0A%0A<b>Rom: </b> <code>$FNAME</code>%0A<b>Date: </b> <code>$BUILD_DATE</code>%0A<b>Size: </b> <code>$size</code>%0A<b>Md5sum: </b> <code>$md5sum</code>%0A<b>Link: </b> <code>$zip</code>" "$CHATID"
-	tg_error "log.txt" "$CHATID"
+	message  "<b>Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)</b>%0A%0A<b>Build Date: </b> <code>$BUILD_DATE</code>%0A<b>Size: </b> <code>$size</code>%0A<b>Md5sum: </b> <code>$md5sum</code>%0A<b>Link: </b> <code>$zip</code>" "$CHATID" > /dev/null
+	error "log.txt" "$CHATID" > /dev/null
      fi
 }
 
 # Build
 build() {
 	source build/envsetup.sh
-	lunch "$ROM"_"$DEVICE"-"$TARGET"
-	make bacon | tee log.txt
+	lunch "$LUNCH"
+	make "$MAKE_TARGET" | tee log.txt
 }
 
 # Checker
 check() {
     if ! [ -f "$OUT"/*2021*.zip ]; then
-        END=$(TZ=Asia/Kolkata date +"%s")
+	END=$(TZ=Asia/Kolkata date +"%s")
 	DIFF=$(( END - START ))
-        tg_post_msg "$FNAME Build for $DEVICE <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!" "$CHATID"
-	tg_post_msg "Check log below" "$CHATID"
-        tg_error "$ERROR_LOG" "$CHATID"
+	message "$ROM_NAME build for $DEVICE <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!" "$CHATID" > /dev/null
+	error "$ERROR_LOG" "$CHATID" > /dev/null
     else
         upload
     fi
 }
 
-# Let's start
-BUILD_DATE="$(TZ=Asia/Kolkata date)"
+# Start
 START=$(TZ=Asia/Kolkata date +"%s")
-tg_post_msg "<b>STARTING BUILD</b>%0A%0A<b>Rom: </b> <code>$FNAME</code>%0A<b>Device: </b> <code>$DEVICE</code>%0A<b>Target: </b> <code>$TARGET</code>%0A<b>Version: </b> <code>$VERSION</code>%0A<b>Build Start: </b> <code>$BUILD_DATE</code>" "$CHATID"
+message "<b>Starting build</b>%0A%0A<b>Rom name: </b> <code>$ROM_NAME</code>%0A<b>Device: </b> <code>$DEVICE</code>%0A<b>Source directory: </b> <code>$(pwd)</code>%0A<b>Make Target: </b> <code>$MAKE_TARGET</code>" "$CHATID" > /dev/null
 
 cleanup
 build
