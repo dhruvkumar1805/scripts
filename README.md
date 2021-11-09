1e763b83b882f269eb122731a58ee3cee86497dd
DhruvChhura <dhruvchhura18@gmail.com>
2021-11-09 07:48:54 +0530

    perosnal_setup: deb_setup.sh: Install some more packages
    * Some minor changes
    * Also update README.md
    
    Signed-off-by: DhruvChhura <dhruvchhura18@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * `build_rom.sh`--> Script for building ROM's, uploads the build to transfer.sh and with Telegram implementation.
 
 * `kernel`--> Script for compiling Android kernels.
+
+* `personal_setup`--> Personal scripts to setup git credentials , build environment and apps after fresh installation.
--- personal_setup/deb-setup.sh
+++ personal_setup/deb-setup.sh
@@ -10,19 +10,13 @@ bldgrn=${txtbld}$(tput setaf 2) #  bold green
 bldblu=${txtbld}$(tput setaf 4) #  bold blue
 txtrst=$(tput sgr0)             #  reset
 
-if [ "$EUID" -ne 0 ];
-then
-        echo -e "$red\nPlease run as root$txtrst\n"
-        exit 1
-fi
-
 echo -e "$grn\nUpdating and installing packages. . .$txtrst\n"
 sudo apt-get update -qq && sudo apt-get upgrade -y
-sudo apt install -qq git gnupg flex bc bison neofetch build-essential zip curl zlib1g-dev gcc-multilib \
+sudo apt install -qq git automake adb fastboot clang lzip apt-utils gnupg libtool subversion flex bc bison neofetch build-essential zip curl zlib1g-dev gcc gcc-multilib \
                         g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev jq \
                         lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig imagemagick \
                         python2 python3 python3-pip python3-dev python-is-python3 schedtool ccache libtinfo5 \
-                        libncurses5 lzop tmux libssl-dev patchelf apktool dos2unix git-lfs default-jdk \
+                        libncurses5 lzop tmux libssl-dev patch patchelf apktool dos2unix git-lfs default-jdk \
                         libxml-simple-perl -y
 
 sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
@@ -30,19 +24,25 @@ sudo chmod a+rx /usr/local/bin/repo
 
 # Git
 if [[ $USER == "dhruv" ]]; then
-echo -e "$grn\nSetting up git. . . $txtrst\n"
+echo -e "$grn\nSetting up git. . .$txtrst\n"
 git config --global user.name "DhruvChhura"
 git config --global user.email "dhruvchhura18@gmail.com"
+fi
 
 # Apps
-echo "$grn\nInstalling Apps. . .$txtrst\n"
+echo -e "$grn\nInstalling Telegram. . .$txtrst\n"
 sudo snap install telegram-desktop
+
+echo -e "$grn\nInstalling VS Code. . .$txtrst\n"
 sudo snap install --classic code
+
+echo -e "$grn\nInstalling Google Chrome. . .$txtrst\n"
 mkdir tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P tmp/
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i tmp/google-chrome-stable_current_amd64.deb
 
-echo "$grn\nCleaning. . .$txtrst\n"
+echo -e "$grn\nCleaning. . .$txtrst\n"
 rm -rf tmp/
+sudo apt-get purge firefox -y
 
-echo "$grnbld\nDone.$txtrst\n"
+echo -e "$grn\nDone.$txtrst"
